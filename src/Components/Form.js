7db79d214202ef3aa3b090645cbcc7287a5a8e35
Shin Yi <contact.shinyi@gmail.com>
2021-11-11 15:00:29 +0800

    added check for price

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,13 +1,9 @@
 import React, { useState } from 'react';
 
-// // function validateEmail(email) {
-// //     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-// //     return re.test(String(email).toLowerCase());
-// // }
-
 export default function Form({handleSubmit}){
 
     const [item, setItem] = useState({name:"",price:"",description:""});
+    const [checkprice, setCheckPrice] = useState(true);
 
     const handleChange = (event) => {
         const label = event.target.name;
@@ -15,24 +11,32 @@ export default function Form({handleSubmit}){
         setItem({...item, [label]:value});
     }
 
+    function checkPrice() {
+        const re = /^\d+\.\d{2}$/;
+        const inputPrice = re.test(item.price);
+        setCheckPrice(inputPrice);
+      }
+
     const handleClick = (event) => {
         event.preventDefault();
-        if (item.name && item.price && item.description) {
-            const newItem = {...item};
-            handleSubmit(newItem);
-            setItem({name:"",price:"",description:""});
+        if (checkprice) {
+            if (item.name && item.price && item.description) {
+                const newItem = {...item};
+                handleSubmit(newItem);
+                setItem({name:"",price:"",description:""});
+            }
         }
     }
 
     return (
         <div>
             <div className="form-signin">
-            <h2 class="form-signin-heading"></h2>
+            <h2 className="form-signin-heading"></h2>
                 <input type="text" name="name" placeholder="product name" value={item.name} onChange={handleChange}/>
-                <input type="text" name="price" placeholder="price" value={item.price} onChange={handleChange}/>
-                {/* <p>Please enter a number</p> */}
+                <input type="text" name="price" placeholder="price" value={item.price} onChange={handleChange} onBlur={checkPrice}/>
                 <input type="text" name="description" placeholder="description" value={item.description} onChange={handleChange}/>
                 <button type="submit" onClick={handleClick}>Submit</button>
+                {checkprice ? <p></p> : <p><span id="warning">Please enter price in number and 2 decimal place</span></p>}
             </div>
         </div>
     )
